20f076a2346068107a68df7501f8f8f87b0c3e79
Alice Swahn <aliceswahn@hotmail.com>
2025-06-05 08:51:21 +0200

    feat: add hardcoded todo list and render it in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useState } from 'react'
 
-function App() {
-  return <></>;
+type Todo = {
+  id: number
+  text: string
+  done: boolean
 }
 
-export default App;
+function App() {
+  const [todos] = useState<Todo[]>([
+    { id: 1, text: 'Läs klart veckans bok', done: false },
+    { id: 2, text: 'Boka tid för vaccination', done: false },
+    { id: 3, text: 'Tvätta bilen', done: false },
+  ])
+
+  return (
+    <main>
+      <h1>Todo-lista</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+export default App
+
